a1d65f106bf76058caaea8449baf5ccc20a917ef
cjh <1418503647@qq.com>
2017-12-13 00:12:40 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="page page-home">
+        <header class="page-header">
+            <div class="container">
+                <h1 class="logo">云设·开源</h1>
+            </div>
+        </header>
+        <div class="banner-box hidden-xs">
+            <div class="container">
+                <div class="row">
+                    <div class="user-bg move-top"></div>
+                    <h1 class="move-right"><a href="#">云设开源起航啦！</a></h1>
+                    <h2 class="move-left"><a href="#">云设首轮拟开源组件列表</a></h2>
+                </div>
+            </div>
+        </div>
+        <main class="page-body">
+            <div class="container">
+
+                <h1 class="section-title">开源项目</h1>
+                <ul class="row project-list">
+                    <li class="col-xs-6 col-ms-6 col-md-4">
+                        <div class="project-item">
+                            <a href="qcloudmna.html" class="open-img" title="标题"><img
+                                    src="/static/img/demo.png" alt="标题"></a>
+                            <h4><a href="qcloudmna.html" title="标题">标题</a></h4>
+                            <p class="info" title="xxx">xxx</p>
+                        </div>
+                    </li>
+                    <li class="col-xs-6 col-ms-6 col-md-4">
+                        <div class="project-item">
+                            1
+                        </div>
+                    </li>
+                    <li class="col-xs-6 col-ms-6 col-md-4">
+                        <div class="project-item">
+                            1
+                        </div>
+                    </li>
+                    <li class="col-xs-6 col-ms-6 col-md-4">
+                        <div class="project-item">
+                            1
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </main>
+        <footer class="page-footer">
+            <div class="copyright">Copyright © 2016 - 2017 Yunser. All Rights Reserved.</div>
+            <div class="copyright">云设团队 版权所有</div>
+        </footer>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+            }
+        }
+    }
+</script>
